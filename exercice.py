6172df5a58ac97063f0cf843386c08a30b8fd402
DESKTOP-2NE278U\Jordan Low <jordan.low@polymtl.ca>
2021-09-13 14:14:34 -0400

    Remise exercices #4

--- exercice.py
+++ exercice.py
@@ -3,23 +3,37 @@
 
 
 def is_even_len(string: str) -> bool:
-    return False
+    is_pair = True
+    if len(str) % 2 == 0:
+        is_pair = True
+    else:
+        is_pair = False
+    return is_pair
 
 
 def remove_third_char(string: str) -> str:
-    return ""
+    new_string = str[0:3:] + str[4::]
+    return new_string
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
+    return str.replace(old_char, new_char)
 
 
-def get_nb_char(string: str, char: str) -> int:
-    return 0
+def get_nb_char(string: str, char: chr) -> int:
+    counter = 0
+    for letter in str:
+        if letter == chr:
+            counter += 1
+    return counter
 
 
 def get_nb_words(sentence: str) -> int:
-    return 0
+    counter = 1
+    for letter in str:
+        if letter == "":
+            counter += 1
+    return counter
 
 
 def main() -> None:
